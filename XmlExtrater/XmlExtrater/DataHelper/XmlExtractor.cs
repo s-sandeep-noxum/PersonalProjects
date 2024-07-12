ecab63ae955fa36757fc9894dae47b426bdd139e
Sandeep Shenoy <s.sandeep@noxum.com>
2024-07-12 11:01:46 +0530

    Xml Extractor from Email

---
+++ XmlExtrater/XmlExtrater/DataHelper/XmlExtractor.cs
@@ -0,0 +1,54 @@
+﻿using System.Xml.Linq;
+
+namespace XmlExtrater.DataHelper
+{
+	public static class XmlExtractor
+	{
+
+		public static Dictionary<string, string>? ExtractXmlTag(string emailText,out int processedPosition)
+		{
+			processedPosition = emailText.Length;
+			Dictionary<string, string> _xmlData = new Dictionary<string, string>();
+			if (string.IsNullOrEmpty(emailText))
+			{
+				return null;
+			}
+
+			if (emailText.Contains("<") && emailText.Contains(">"))
+			{
+				int startIndex = emailText.IndexOf("<");
+				int endIndex = emailText.IndexOf(">");
+				string tag = emailText.Substring(startIndex + 1, endIndex - startIndex - 1);
+
+				if (!emailText.Contains("</" + tag + ">"))
+				{
+					return null;
+				}
+
+				processedPosition = emailText.IndexOf("</" + tag + ">") + ("</" + tag + ">").Length;
+
+				string value = emailText.Substring(emailText.IndexOf("<" + tag + ">"), emailText.IndexOf("</" + tag + ">") - emailText.IndexOf("<" + tag + ">") + ("</" + tag + ">").Length);
+
+
+				var xmlData = XElement.Parse(value);
+				if (xmlData.Descendants().Count() > 0)
+				{
+					foreach (var item in xmlData.Descendants())
+					{
+						_xmlData.Add(item.Name.LocalName, item.Value);
+					}
+				}
+				else
+				{
+					_xmlData.Add(xmlData.Name.LocalName, xmlData.Value);
+				}
+			}
+			else
+			{
+				return null;
+			}
+
+			return _xmlData;
+		}
+	}
+}
---
+++ XmlExtrater/XmlExtrater/MainWindow.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections.Generic;
+using System.Windows;
+
+namespace XmlExtrater
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		public Dictionary<string, string> CompleteXmlList { get; set; } = new Dictionary<string, string>();
+
+		private void btnStripXml_Click(object sender, RoutedEventArgs e)
+		{
+			string emailText = rtbData.Text;
+			Dictionary<string, string>? xmlData = new Dictionary<string, string>();
+			int processedPosition = 0;
+			do
+			{
+				xmlData = XmlExtrater.DataHelper.XmlExtractor.ExtractXmlTag(emailText, out processedPosition);
+
+				if (xmlData != null)
+				{
+					foreach (var item in xmlData)
+					{
+						CompleteXmlList.Add(item.Key, item.Value);
+					}
+				}
+				emailText = emailText.Substring(processedPosition);
+			} while (emailText.Length > 0);
+
+			rtbXmlData.Text = string.Join(Environment.NewLine, CompleteXmlList);
+		}
+	}
+}
