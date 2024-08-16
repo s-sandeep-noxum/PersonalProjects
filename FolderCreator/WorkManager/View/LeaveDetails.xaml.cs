2d6967db289d245570094f16681ea0a104843c34
Sandeep Shenoy <s.sandeep@noxum.com>
2024-08-16 23:44:33 +0530

    Add Leave Details and format xaml

--- FolderCreator/WorkManager/View/LeaveDetails.xaml.cs
+++ FolderCreator/WorkManager/View/LeaveDetails.xaml.cs
@@ -22,7 +22,7 @@ namespace WorkManager.View
 	{
 		public LeaveDetails()
 		{
-			InitializeComponent();
+			InitializeComponent();			
 		}
 	}
 }
---
+++ FolderCreator/WorkManager/ViewModel/LeaveDetailsViewModel.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+
+namespace WorkManager.ViewModel
+{
+	public class LeaveDetailsViewModel : INotifyPropertyChanged
+	{
+		private DateTime date;
+
+		private string typeOfLeave;
+
+		public event PropertyChangedEventHandler PropertyChanged;
+
+		public LeaveDetailsViewModel()
+		{
+			Date = DateTime.Now;			
+		}
+		public DateTime Date
+		{
+			get { return date; }
+			set
+			{
+				if (date != value)
+				{
+					date = value;
+					OnPropertyChanged(nameof(Date));
+				}
+			}
+		}
+
+		public string TypeOfLeave
+		{
+			get
+			{
+				return typeOfLeave;
+			}
+			set
+			{
+				if (typeOfLeave != value)
+				{
+					typeOfLeave = value;
+					OnPropertyChanged(nameof(TypeOfLeave));
+				}
+			}
+		}
+
+		public List<string> TypeOfLeaves => new List<string>
+						{
+							"Casual Leave",
+							"Sick Leave",
+							"Optional Leave"
+						};
+		private void OnPropertyChanged(string propertyName)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+	}
+}
