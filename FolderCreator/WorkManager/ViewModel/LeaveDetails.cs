62ef10f7b9e16aca2f2b0c7e0786aa4ca89c0695
Sandeep Shenoy <s.sandeep@noxum.com>
2024-08-21 16:46:46 +0530

    Add Context menu for exporting leave details.

--- FolderCreator/WorkManager/ViewModel/LeaveDetails.cs
+++ FolderCreator/WorkManager/ViewModel/LeaveDetails.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Input;
+using WorkManager.Commands;
 
 namespace WorkManager.ViewModel
 {
@@ -15,5 +17,28 @@ namespace WorkManager.ViewModel
 		public string Comment { get; set; }
 		public string Month { get; set; }
 		public string Day { get; set; }
+		private ICommand exportCommand;
+
+		public bool CanExecute
+		{
+			get
+			{
+				// check if executing is allowed, i.e., validate, check if a process is running, etc.
+				return true;
+			}
+		}
+
+		public ICommand ExportCommand
+		{
+			get
+			{
+				return exportCommand ?? (exportCommand = new RelayCommand(PrintLeaves));
+			}
+		}
+
+		private void PrintLeaves(object obj)
+		{
+			throw new NotImplementedException();
+		}
 	}
 }
